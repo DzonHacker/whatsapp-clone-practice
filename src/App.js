2469040f3a9d16f3aa6cf0a41dfab957e1073efd
John Sherchan <john.sherchan10@gmail.com>
2021-01-28 14:15:42 +0545

    new proj

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import Sidebar from './Components/Sidebar'
+
+function App() {
+  return (
+    <div className="app">
+      <div className="app__body">
+        {/* Slidebar */}
+        {/* Chat */}
+        <Sidebar/>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyAnWGCObHnUZEbLVoqoYOvX6GCbkcSe0zs",
+    authDomain: "whatsapp-clone-practice-6bcd4.firebaseapp.com",
+    projectId: "whatsapp-clone-practice-6bcd4",
+    storageBucket: "whatsapp-clone-practice-6bcd4.appspot.com",
+    messagingSenderId: "646337670497",
+    appId: "1:646337670497:web:d594142551a748953c6920",
+    measurementId: "G-82HSPSEQVY"
+  };
